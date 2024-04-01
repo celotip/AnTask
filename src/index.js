a1b1daad57058f4e859da7a5ee50b38d9c69ac42
celotip <lino.marcel148@email.com>
2024-04-01 19:30:41 +0800

    added week

--- src/index.js
+++ src/index.js
@@ -55,71 +55,73 @@ function capitalize(s)
 
 
 
+// sidebar
 
-// Sidebar
+const menuButton = document.getElementById("menu");
+const sidebar = document.querySelector(".sidebar");
 
-// const menuButton = document.getElementById("menu");
-// const sidebar = document.querySelector(".sidebar");
-
-// function openNav() {
-//     const sidebarWidth = "200px";
-//     document.getElementById("mySidebar").style.width = sidebarWidth;
-//     document.getElementById("content").style.paddingLeft = sidebarWidth;
-// }
+function openNav() {
+    const sidebarWidth = "200px";
+    document.getElementById("mySidebar").style.width = sidebarWidth;
+    document.getElementById("content").style.paddingLeft = sidebarWidth;
+}
   
-// function closeNav() {
-//     document.getElementById("mySidebar").style.width = "0";
-//     document.getElementById("content").style.paddingLeft= "0";
-// }
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("content").style.paddingLeft= "0";
+}
 
-// var divs = document.querySelectorAll(".main-container > :not(.sidebar)");
+var divs = document.querySelectorAll(".main-container > :not(.sidebar)");
 
-// divs.forEach((div) => {
-//     div.addEventListener("click", () => {
-//         if (screen.width < 500 && sidebar.classList.contains("open")) {
-//             closeNav();
-//             sidebar.classList.remove("open");
-//             console.log("close");
-//         }
-//     });
-// });
+divs.forEach((div) => {
+    div.addEventListener("click", () => {
+        if (screen.width < 500 && sidebar.classList.contains("open")) {
+            closeNav();
+            sidebar.classList.remove("open");
+            console.log("close");
+        }
+    });
+});
 
-// function addOpen() {
-//     sidebar.classList.add("open");
-// }
+function addOpen() {
+    sidebar.classList.add("open");
+}
 
-// menuButton.addEventListener("click", () => {
-//     if (!sidebar.classList.contains("open")) {
-//         openNav();
-//         setTimeout(addOpen, 100);
-//         console.log("open");
-//     } else {
-//         closeNav();
-//         sidebar.classList.remove("open");
-//     }
-// });
+menuButton.addEventListener("click", () => {
+    if (!sidebar.classList.contains("open")) {
+        openNav();
+        setTimeout(addOpen, 100);
+        console.log("open");
+    } else {
+        closeNav();
+        sidebar.classList.remove("open");
+    }
+});
 
-// const today = document.getElementById("today");
-// const addTag = document.getElementById("add-tag");
+const today = document.getElementById("today");
+const addTag = document.getElementById("add-tag");
 
-// today.addEventListener("click", () => {
-//     content.classList.remove("bg");
-//     content.classList.add("bg-none");
-//     content.innerHTML = "";
-//     content.appendChild(home());
-// })
+today.addEventListener("click", () => {
+    content.classList.remove("bg");
+    content.classList.add("bg-none");
+    content.innerHTML = "";
+    content.appendChild(home());
+    addTodayListeners();
+    addAddTaskListeners();
+})
 
-// // const week = document.getElementById("week");
-// // week.addEventListener("click", () => {
-// //     content.classList.remove("bg-none");
-// //     content.classList.add("bg");
-// //     content.innerHTML = "";
-// //     content.appendChild(weekView());
-// // })
+const week = document.getElementById("week");
+week.addEventListener("click", () => {
+    content.classList.remove("bg-none");
+    content.classList.add("bg");
+    content.innerHTML = "";
+    content.appendChild(weekView());
+    addWeekListeners();
+})
 
-// addTag.addEventListener("click", () => {
-//     tagPopup.style.visibility = "visible";
-// })
+addTag.addEventListener("click", () => {
+    tagPopup.style.visibility = "visible";
+})
 
 
 
@@ -273,6 +275,13 @@ function home() {
     const newTaskButton = document.createElement("button");
     newTaskButton.classList.add("new-task");
     newTaskButton.innerHTML = "New Task";
+    newTaskButton.addEventListener("click", () => {
+        newTaskPopup.style.visibility = "visible";
+        newTitle.value = "";
+        newDesc.value = "";
+        newDue.value = ""; 
+        newTitle.focus();
+    })
     todayTasksContainer1.appendChild(displayTodayTasks());
     todayTasksContainer.appendChild(todayTasksContainer1);
     homeContainer.appendChild(todayTasksContainer);
@@ -284,11 +293,113 @@ function home() {
     return homeContainer;
 };
 
+function createWeekCardTask(taskName, taskDue, done) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("data-name", taskName);
+    const check = document.createElement("div");
+    check.classList.add("check");
+    check.setAttribute("data-name", taskName);
+    if (done) {
+        card.classList.add("done");
+        check.classList.add("check-done");
+        check.innerHTML = "&#10003;";
+    }
+    const name = document.createElement("div");
+    name.classList.add("name");
+    name.innerHTML = taskName;
+    const due = document.createElement("div");
+    due.classList.add("due");
+    due.innerHTML = format(taskDue, "MMM dd");
+    const del = document.createElement("i");
+    del.classList.add("fa-trash-o");
+    del.classList.add("fa");
+    del.classList.add("del");
+    del.setAttribute("data-name", taskName);
+    card.appendChild(check);
+    card.appendChild(name);
+    card.appendChild(due);
+    card.appendChild(del);
+
+    return card;
+}
+
+function displayToday() {
+    const tasksContainer = document.createElement("div");
+    tasksContainer.classList.add("tasks-container");
+    const curDate = new Date();
+    const todayTasks = TasksList.list.filter((task) => {
+        return new Date(task.due) <= curDate;
+    })
+    todayTasks.forEach((task) => {
+        tasksContainer.appendChild(createWeekCardTask(task.name, task.due, task.done))
+    })
+    return tasksContainer;
+}
+
+function displayWeek() {
+    const tasksContainer = document.createElement("div");
+    tasksContainer.classList.add("tasks-container");
+    const nextWeek = addDays(new Date(), 7)
+    const todayTasks = TasksList.list.filter((task) => {
+        return new Date(task.due) <= nextWeek && new Date(task.due) > new Date();
+    })
+    todayTasks.forEach((task) => {
+        tasksContainer.appendChild(createWeekCardTask(task.name, task.due, task.done))
+    })
+    return tasksContainer;
+}
+
+function displayLater() {
+    const tasksContainer = document.createElement("div");
+    tasksContainer.classList.add("tasks-container");
+    const nextWeek = addDays(new Date(), 7)
+    const todayTasks = TasksList.list.filter((task) => {
+        return new Date(task.due) > nextWeek;
+    })
+    todayTasks.forEach((task) => {
+        tasksContainer.appendChild(createWeekCardTask(task.name, task.due, task.done))
+    })
+    return tasksContainer;
+}
+
 function weekView() {
+    const weekViewContainer = document.createElement("div");
+    weekViewContainer.classList.add("week");
+    weekViewContainer.classList.add("container");
+    const todayHeading = document.createElement("h1")
+    todayHeading.innerHTML = "Due Today"
+    const todayContainer = document.createElement("div");
+    todayContainer.classList.add("today-container");
+    todayContainer.appendChild(displayToday());
+    const weekHeading = document.createElement("h1")
+    weekHeading.innerHTML = "Due this Week"
     const weekContainer = document.createElement("div");
-    weekContainer.classList.add("week");
-    weekContainer.classList.add("container");
-    return weekContainer;
+    weekContainer.classList.add("week-container");
+    weekContainer.appendChild(displayWeek());
+    const laterHeading = document.createElement("h1")
+    laterHeading.innerHTML = "Due Later"
+    const laterContainer = document.createElement("div");
+    laterContainer.classList.add("later-container");
+    laterContainer.appendChild(displayLater());
+    const newTaskButton = document.createElement("button");
+    newTaskButton.classList.add("week-new-task");
+    newTaskButton.innerHTML = "+";
+    newTaskButton.addEventListener("click", () => {
+        newTaskPopup.style.visibility = "visible";
+        newTitle.value = "";
+        newDesc.value = "";
+        newDue.value = ""; 
+        newTitle.focus();
+    })
+    weekViewContainer.append(todayHeading)
+    weekViewContainer.append(todayContainer)
+    weekViewContainer.append(weekHeading)
+    weekViewContainer.append(weekContainer)
+    weekViewContainer.append(laterHeading)
+    weekViewContainer.append(laterContainer)
+    weekViewContainer.appendChild(newTaskButton)
+    return weekViewContainer;
 };
 
 
@@ -333,22 +444,27 @@ const newDesc = document.getElementById("new-desc");
 const newDue = document.getElementById("new-due");
 const newTaskPopup = document.querySelector(".new-task-popup");
 
-const newTaskButton = document.querySelector(".new-task");
-newTaskButton.addEventListener("click", () => {
-    newTaskPopup.style.visibility = "visible";
-    newTitle.value = "";
-    newDesc.value = "";
-    newDue.value = ""; 
-})
-
 const newForm = document.querySelector(".new-task-popup > form");
 newForm.addEventListener("submit", (e) => {
     e.preventDefault();
     TasksList.list.push(new Task(newTitle.value, newDesc.value, newDue.value, false));
     updateStorage();
-    updateAddTasks();
+    if (document.querySelector(".home")) {
+        updateAddTasks();
+    } else {
+        updateWeekTasks();
+    }
     newTaskPopup.style.visibility = "hidden";
 })
+
+// function handleEnter(event) {
+//     if (event.key==="Enter") {
+//        const form = document.getElementById('form')
+//        const index = [...form].indexOf(event.target);
+//        form.elements[index + 1].focus();
+//        //event.preventDefault();
+//      }
+//  }
 
 const cancel = document.querySelector(".new-task-popup > form > .buttons-container > .cancel");
 cancel.addEventListener("click", () => {
@@ -359,18 +475,45 @@ function updateTodayTasks() {
     const todayTasksContainer1 = document.querySelector(".today-tasks-container-1");
     todayTasksContainer1.innerHTML = "";
     todayTasksContainer1.appendChild(displayTodayTasks());
+    addTodayListeners()
+}
+
+function addTodayListeners() {
     addRemoveListener();
     addDoneListener();
-    addCardListener()
+    addCardListenerTodayTasks()
+}
+
+function addWeekListeners() {
+    addDelListener();
+    addDoneListener();
+    addCardListenerWeek()
 }
 
 function updateAddTasks() {
     const addTasksContainer = document.querySelector(".add-tasks");
     addTasksContainer.innerHTML = "";
     addTasksContainer.appendChild(addTasks());
+    addAddTaskListeners()
+}
+
+function updateWeekTasks() {
+    const todayContainer = document.querySelector(".today-container");
+    todayContainer.innerHTML = "";
+    todayContainer.appendChild(displayToday());
+    const weekContainer = document.querySelector(".week-container");
+    weekContainer.innerHTML = "";
+    weekContainer.appendChild(displayWeek());
+    const laterContainer = document.querySelector(".later-container");
+    laterContainer.innerHTML = "";
+    laterContainer.appendChild(displayLater());
+    addWeekListeners();
+}
+
+function addAddTaskListeners() {
     addAddListener();
     addDelListener();
-    addCardListener();
+    addCardListenerAddTasks();
 }
 
 function addRemoveListener() {
@@ -452,7 +595,11 @@ function addDelListener() {
                 return task.name !== button.dataset.name;
             })
             updateStorage();
-            updateAddTasks();
+            if (document.querySelector(".home")) {
+                updateAddTasks();
+            } else {
+                updateWeekTasks();
+            }
         })
     })    
 }
@@ -465,8 +612,8 @@ function updateStorage() {
 
 const cardPopup = document.querySelector(".card-popup");
 
-function addCardListener() {
-    const cards = document.querySelectorAll(".card");
+function addCardListenerAddTasks() {
+    const cards = document.querySelectorAll(".add-tasks .card");
     cards.forEach((card) => {
         card.addEventListener("click", () => {
             cardPopup.style.visibility = "visible";
@@ -481,38 +628,86 @@ function addCardListener() {
             title.innerHTML = tasks[0].name;
             desc.innerHTML = tasks[0].notes;
             due.innerHTML = format(tasks[0].due, "MMM dd yyyy");
-        })
+            console.log("card clicked")
+        }, {once: true})
     })
 }
 
-addCardListener();
+function addCardListenerTodayTasks() {
+    const cards = document.querySelectorAll(".today-tasks-container-1 .tasks-container .card");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            cardPopup.style.visibility = "visible";
+            const edit = document.querySelector(".edit");
+            edit.dataset.name = card.dataset.name;
+            const title = document.getElementById("card-title");
+            const desc = document.getElementById("card-desc");
+            const due = document.getElementById("card-due");
+            const tasks = TasksList.list.filter((task) => {
+                return task.name === card.dataset.name;
+            })
+            title.innerHTML = tasks[0].name;
+            desc.innerHTML = tasks[0].notes;
+            due.innerHTML = format(tasks[0].due, "MMM dd yyyy");
+            console.log("card clicked")
+        }, {once: true})
+    })
+}
+
+function addCardListenerWeek() {
+    const cards = document.querySelectorAll(".week .card");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            cardPopup.style.visibility = "visible";
+            const edit = document.querySelector(".edit");
+            edit.dataset.name = card.dataset.name;
+            const title = document.getElementById("card-title");
+            const desc = document.getElementById("card-desc");
+            const due = document.getElementById("card-due");
+            const tasks = TasksList.list.filter((task) => {
+                return task.name === card.dataset.name;
+            })
+            title.innerHTML = tasks[0].name;
+            desc.innerHTML = tasks[0].notes;
+            due.innerHTML = format(tasks[0].due, "MMM dd yyyy");
+            console.log("card clicked")
+        }, {once: true})
+    })
+}
+
+
+addCardListenerAddTasks();
+addCardListenerTodayTasks();
 
 const editTaskPopup = document.querySelector(".edit-task-popup");
 const editForm = document.querySelector(".edit-task-popup > form");
 const editTitle = document.getElementById("edit-title");
 const editDesc = document.getElementById("edit-desc");
 const editDue = document.getElementById("edit-due");
+let editName;
 
 const cancelEdit = document.querySelector(".edit-task-popup > form > .buttons-container > .cancel");
 cancelEdit.addEventListener("click", () => {
     editTaskPopup.style.visibility = "hidden";
 })
 
-function addEditListener(name) {
-    editForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const tasks = TasksList.list.filter((task) => {
-            return task.name === name;
-        })
-        tasks[0].name = editTitle.value;
-        tasks[0].notes = editDesc.value;
-        tasks[0].due = editDue.value;
-        updateStorage();
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const tasks = TasksList.list.filter((task) => {
+        return task.name === editName;
+    })
+    tasks[0].name = editTitle.value;
+    tasks[0].notes = editDesc.value;
+    tasks[0].due = editDue.value;
+    if (document.querySelector(".home")) {
         updateTodayTasks();
         updateAddTasks();
-        editTaskPopup.style.visibility = "hidden";
-    })
-}
+    } else {
+        updateWeekTasks();
+    }
+    updateStorage();
+    editTaskPopup.style.visibility = "hidden";
+})
 
 const closeButton = document.querySelector(".close");
 closeButton.addEventListener("click", () => {
@@ -529,5 +724,5 @@ editButton.addEventListener("click", () => {
     editTitle.value = tasks[0].name;
     editDesc.value = tasks[0].notes;
     editDue.value = tasks[0].due;
-    addEditListener(editButton.dataset.name);
+    editName = editButton.dataset.name;
 })
